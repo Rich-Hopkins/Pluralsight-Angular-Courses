df49cf1a30629a343a75576a5545ddbeced5f072
Rich <hopmedic@hotmail.com>
2017-03-29 02:21:55 -0400

    Mostly through services, about to write GitHub service.

--- AngularJS Get Started - Scott Allen/01-Project/app.js
+++ AngularJS Get Started - Scott Allen/01-Project/app.js
@@ -4,7 +4,7 @@
 (function () {
   var app = angular.module("githubViewer", []);
 
-  var MainController = function ($scope, $http) {
+  var MainController = function ($scope, $http, $interval, $log, $anchorScroll, $location) {
 
     var onUserComplete = function (response) {
       $scope.user = response.data;
@@ -14,21 +14,42 @@
 
     var onRepos = function (response) {
       $scope.repos = response.data;
+      $location.hash("userDetails");
+      $anchorScroll();
     };
 
     var onError = function (reason) {
       $scope.error = "Could not fetch data.";
     };
 
+    var decrementCountdown = function(){
+      $scope.countdown -= 1;
+      if($scope.countdown < 1) {
+        $scope.search($scope.username);
+      }
+    };
+
+    var countdownInterval = null;
+    var startCountdown = function() {
+      countdownInterval = $interval(decrementCountdown, 1000, $scope.countdown);
+    };
+
     $scope.search = function (username) {
+      $log.info("Searching for " + username);
       $http.get("https://api.github.com/users/" + username)
           .then(onUserComplete, onError);
+      if(countdownInterval) {
+        $interval.cancel(countdownInterval);
+        $scope.countdown = null;
+      }
     };
 
     $scope.username = "angular";
     $scope.message = "GitHub Viewer";
     $scope.repoSortOrder = "-stargazers_count";
+    $scope.countdown = 5;
+    startCountdown();
   };
 
-  app.controller("MainController", ["$scope", "$http", MainController]);
+  app.controller("MainController", ["$scope", "$http", "$interval", "$log", "$anchorScroll", "$location", MainController]);
 }());
