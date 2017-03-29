e4b27abd5f6a8fe8e818ad0351014069f21b44cf
Rich <hopmedic@hotmail.com>
2017-03-29 02:47:59 -0400

    Finished Services module.

--- AngularJS Get Started - Scott Allen/01-Project/app.js
+++ AngularJS Get Started - Scott Allen/01-Project/app.js
@@ -4,16 +4,15 @@
 (function () {
   var app = angular.module("githubViewer", []);
 
-  var MainController = function ($scope, $http, $interval, $log, $anchorScroll, $location) {
+  var MainController = function ($scope, github, $interval, $log, $anchorScroll, $location) {
 
-    var onUserComplete = function (response) {
-      $scope.user = response.data;
-      $http.get($scope.user.repos_url)
-          .then(onRepos, onError);
+    var onUserComplete = function(data) {
+      $scope.user = data;
+      github.getRepos($scope.user).then(onRepos, onError);
     };
 
-    var onRepos = function (response) {
-      $scope.repos = response.data;
+    var onRepos = function (data) {
+      $scope.repos = data;
       $location.hash("userDetails");
       $anchorScroll();
     };
@@ -36,8 +35,7 @@
 
     $scope.search = function (username) {
       $log.info("Searching for " + username);
-      $http.get("https://api.github.com/users/" + username)
-          .then(onUserComplete, onError);
+      github.getUser(username).then(onUserComplete, onError);
       if(countdownInterval) {
         $interval.cancel(countdownInterval);
         $scope.countdown = null;
@@ -51,5 +49,5 @@
     startCountdown();
   };
 
-  app.controller("MainController", ["$scope", "$http", "$interval", "$log", "$anchorScroll", "$location", MainController]);
+  app.controller("MainController", MainController);
 }());
