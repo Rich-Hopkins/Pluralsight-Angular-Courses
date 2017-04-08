3c12c5b81f2f2e4b414b8ae32fcd6b0bdb2d251e
Rich <hopmedic@hotmail.com>
2017-04-08 13:58:59 -0400

    Saving Data with $resource

--- AngularFundamentals/Events/app/js/controllers/EditEventController.js
+++ AngularFundamentals/Events/app/js/controllers/EditEventController.js
@@ -3,14 +3,23 @@
  */
 'use strict'
 
-eventsApp.controller('EditEventController', function EditEventController($scope) {
-  $scope.saveEvent = function (event, newEventForm) {
-    if (newEventForm.$valid) {
-      window.alert("save was clicked");
+eventsApp.controller('EditEventController', function EditEventController($scope, eventData){
+
+  $scope.saveEvent = function(event, newEventForm){
+    if(newEventForm.$valid){
+      eventData.save(event)
+               .$promise
+               .then(function(response){
+                 console.log('success', response)
+               })
+               .catch(function(response){
+                 console.log('failure', response)
+               });
+
     }
   };
 
-  $scope.cancelEdit = function () {
+  $scope.cancelEdit = function(){
     window.location = "/EventDetails.html";
   };
 });
--- AngularFundamentals/Events/app/js/services/EventData.js
+++ AngularFundamentals/Events/app/js/services/EventData.js
@@ -2,9 +2,14 @@
  * Created by Rich Hopkins on 4/4/2017.
  */
 eventsApp.factory('eventData', function($resource){
+  var resource = $resource('/data/event/:id', {id:'@id'});
   return {
     getEvent: function(){
-      return $resource('/data/event/:id', {id:'@id'}).get({id:1});
+      return resource.get({id:1});
+    },
+    save:function(event){
+      event.id=999;
+      return resource.save(event);
     }
   };
 });
