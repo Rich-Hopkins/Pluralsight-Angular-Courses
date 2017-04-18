9fe40253b3d9fa21745d4af577e9c936496fee2e
Rich <hopmedic@hotmail.com>
2017-04-17 23:32:44 -0400

    Completed custom directives module

---
+++ AngularFundamentals/Events/app/js/directives/datepicker.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Rich Hopkins on 4/17/2017.
+ */
+eventsApp.directive('datePicker', function(){
+  return {
+    restrict 'A',
+    link: function(scope, element){
+      element.datepicker();
+    }
+  }
+});
