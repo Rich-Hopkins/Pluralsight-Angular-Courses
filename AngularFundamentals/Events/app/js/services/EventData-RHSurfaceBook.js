92d79438f170fcc8b2f67fc0ba1827efa99afdca
Rich <hopmedic@hotmail.com>
2017-05-12 01:03:20 -0400

    correcting git mistakes

---
+++ AngularFundamentals/Events/app/js/services/EventData-RHSurfaceBook.js
@@ -0,0 +1,20 @@
+eventsApp.factory('eventData', function($resource){
+  var resource = $resource('/eventsdata/event/:id', {id: '@id'}, {
+    "getAll": {
+      method: "GET",
+      isArray: true
+    }
+  });
+  return {
+    getEvent: function(eventId){
+      return resource.get({id:eventId});
+    },
+    save: function(event){
+      event.id = 999;
+      return resource.save(event);
+    },
+    getAllEvents: function(){
+      return resource.query();
+    }
+  };
+});
