22a65ee2e1433f3955fee5613b01ff55d94096c2
Rich <hopmedic@hotmail.com>
2017-05-27 17:21:16 -0400

    Configured $routeProvider.

--- SchoolBuddy/public/app/app.js
+++ SchoolBuddy/public/app/app.js
@@ -1,11 +1,34 @@
-(function() {
+(function(){
 
-    var app = angular.module('app', []);
+  var app = angular.module('app', ['ngRoute']);
 
-    app.config(['$logProvider', function ($logProvider) {
+  app.config(['$logProvider', '$routeProvider', function($logProvider, $routeProvider){
 
-        $logProvider.debugEnabled(true);
+    $logProvider.debugEnabled(true);
 
-    }]);
+    $routeProvider
+      .when('/', {
+        controller: 'HomeController',
+        controllerAs: 'home',
+        templateUrl: 'app/templates/home.html'
+      })
+      .when('/schools', {
+        controller: 'AllSchoolsController',
+        controllerAs: 'schools',
+        templateUrl: 'app/templates/allSchools.html'
+      })
+      .when('/classrooms/:id', {
+        controller: 'AllClassroomsController',
+        controllerAs: 'classrooms',
+        templateUrl: 'app/templates/allClassrooms.html'
+      })
+      .when('/activities', {
+        controller: 'AllActivitiesController',
+        controllerAs: 'activities',
+        templateUrl: 'app/templates/allActivities.html'
+      })
+      .otherwise('/');
+
+  }]);
 
 }());
